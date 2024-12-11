fd1a2ed2ddf8fe00c500f8da68362a50481abfc0
nenosoft131 <usman.butt131@gmail.com>
2024-12-11 15:01:41 +0100

    setup_working

--- setup.py
+++ setup.py
@@ -2,23 +2,22 @@ from setuptools import find_packages, setup
 from typing import List
 
 HYPEN_E = '-e .'
-def get_requirements(fpath:str)->List[str]:
-    
-    requirements=[]
+
+def get_requirements(fpath: str) -> List[str]:
+    requirements = []
     with open(fpath) as fobj:
-        requirements =fobj.readline()
-        requirements= [req.replace("\n","") for req in requirements]
+        requirements = fobj.readlines()
+        requirements = [req.strip() for req in requirements]
         
         if HYPEN_E in requirements:
             requirements.remove(HYPEN_E)
     
     return requirements
 
-
 setup(
-    name = 'MlPro',
-    version= '0.1',
-    author= 'Muhammad',
-    packages= find_packages(),
-    install_requires= get_requirements('requirements.txt')
+    name='mlPro',
+    version='0.0.1',
+    author='Muhammad',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
