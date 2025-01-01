6065614dcbf37146a1ac288da23bbf349db2dab0
Muhammad <muhammad-usman.butt@stud-mail.uni-wuerzburg.de>
2025-01-01 19:34:02 +0100

    Tested Exceptions and Logs

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from datetime import datetime
+from exception import CustomException
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
@@ -15,3 +16,7 @@ logging.basicConfig(
 
 
 )
+
+if __name__ == '__main__':
+    logging.info("ppaap")
+    
