88f4a42a9b0b97c087dcb9c857e5101d4849eebd
MUHAMMAD USMAN <74011529+nenosoft131@users.noreply.github.com>
2025-02-19 18:06:27 +0100

    Update helper.py

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -3,10 +3,3 @@ def encode_to_ascii(string):
     for char in string:
         encoded_string += str(ord(char)) + " "
     return encoded_string.strip()
-
-def decode_from_ascii(encoded_string):
-    decoded_string = ""
-    ascii_list = encoded_string.split()
-    for ascii_code in ascii_list:
-        decoded_string += chr(int(ascii_code))
-    return decoded_string
