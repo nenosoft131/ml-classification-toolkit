14bd0ea9490e75c62738d17b0d05b7bfbbe855b2
MUHAMMAD USMAN <74011529+nenosoft131@users.noreply.github.com>
2025-02-28 11:54:19 +0100

    Update utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,65 +1,46 @@
-
 import os
 import sys
-
 import numpy as np 
 import pandas as pd
 import dill
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
-
 from src.exception import CustomException
 
-#update model evalution functions
+# Function to save an object to a file
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
-
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
-
     except Exception as e:
         raise CustomException(e, sys)
     
-    #update model evalution functions
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+# Function to evaluate models with GridSearchCV and return a report
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
-
-        for i in range(len(list(models))):
-            model = list(models.values())[i]s
-            para=param[list(models.keys())[i]]
-
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
-
+        for model_name, model in models.items():
+            para = param[model_name]
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
             model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
-
-            #model.fit(X_train, y_train)  # Train model
-
+            model.fit(X_train, y_train)
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
-
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
-
-            report[list(models.keys())[i]] = test_model_score
-
+            report[model_name] = test_model_score
         return report
-
     except Exception as e:
         raise CustomException(e, sys)
     
+# Function to load an object from a file
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
             return pickle.load(file_obj)
-
     except Exception as e:
         raise CustomException(e, sys)
