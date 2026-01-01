2f82866b9eec5f68309813fc01749e1502fb6730
MUHAMMAD USMAN <74011529+nenosoft131@users.noreply.github.com>
2026-01-01 17:45:08 +0100

    Rename RAMAN_YAXIS to DATA_YAXIS in config.py

--- src/streamlit_app/config.py
+++ src/streamlit_app/config.py
@@ -7,13 +7,13 @@ FILEPATH_LOGO_JPG = "icon.jpeg"
 
 # NUM_FREQUENCIES = 1024
 NUM_FREQUENCIES = 882
-RAMAN_YAXIS_MAX = 20000
-RAMAN_YAXIS_TYPE = 'linear'  # ['linear', 'log']
+DATA_YAXIS_MAX = 20000
+DATA_YAXIS_TYPE = 'linear'  # ['linear', 'log']
 
 backend_scatter = 'plotly'
 
 groups = [
     'malicious',
-    'ransom'
+    'infectious'
 ]
 
