2ad6e3fc533c14cf1e98a1544c96e78417be9844
MUHAMMAD USMAN <74011529+nenosoft131@users.noreply.github.com>
2026-01-01 17:44:23 +0100

    Change logo file and update groups in config
    Updated logo file name and modified groups list.

--- src/streamlit_app/config.py
+++ src/streamlit_app/config.py
@@ -3,7 +3,7 @@ data_root = "./data/"
 
 datasets = ['old_data']
 
-FILEPATH_LOGO_JPG = "logo.jpeg"
+FILEPATH_LOGO_JPG = "icon.jpeg"
 
 # NUM_FREQUENCIES = 1024
 NUM_FREQUENCIES = 882
@@ -13,14 +13,7 @@ RAMAN_YAXIS_TYPE = 'linear'  # ['linear', 'log']
 backend_scatter = 'plotly'
 
 groups = [
-    'ecoli',
-    'ecoli_ringer',
-    'ecoli_new',
-    'brevis_ringer',
-    'omnibio',
-    'notarget_dirty',
-
-    # 'notarget_noanalyte',
-    # 'notarget_ringer'
+    'malicious',
+    'ransom'
 ]
 
